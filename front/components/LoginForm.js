19f34df0a8d560ad3f77f890dfd5a1461e6eea35
seilylook <seilylook@naver.com>
2023-02-04 13:17:14 +0900

    로그인 폼 padding design 해서 여유 주기

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -7,6 +7,10 @@ const ButtonWrapper = styled.div`
   margin-top: 10px;
 `;
 
+const FormWrapper = styled(Form)`
+  padding: 10px;
+`;
+
 const LoginForm = ({ setIsLoggedIn }) => {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
@@ -26,7 +30,7 @@ const LoginForm = ({ setIsLoggedIn }) => {
   }, [id, password]);
 
   return (
-    <Form onFinish={onSubmitForm}>
+    <FormWrapper onFinish={onSubmitForm}>
       <div>
         <label htmlFor='user_id'>아이디</label>
         <br />
@@ -53,7 +57,7 @@ const LoginForm = ({ setIsLoggedIn }) => {
           </a>
         </Link>
       </ButtonWrapper>
-    </Form>
+    </FormWrapper>
   );
 };
 
