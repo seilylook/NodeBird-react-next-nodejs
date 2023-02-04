838ca11fa2b5dd36cffd2164ad659e3ea4e4b84f
seilylook <seilylook@naver.com>
2023-02-04 12:46:39 +0900

    with setIsloggedIn(dummy data) UserProfile/LoginForm finish

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -40,7 +40,7 @@ const AppLayout = ({ children }) => {
       <Row gutter={8}>
         <Col xs={24} md={6}>
           {isLoggedIn ? (
-            <UserProfile />
+            <UserProfile setIsloggedIn={setIsLoggedIn} />
           ) : (
             <LoginForm setIsLoggedIn={setIsLoggedIn} />
           )}
--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -1,6 +1,37 @@
-import React from 'react';
-import { Card } from 'antd';
+import React, { useCallback } from 'react';
+import { Card, Avatar, Button } from 'antd';
+import styled from 'styled-components';
 
-const UserProfile = () => {};
+const ButtonWrapper = styled(Button)`
+  margin-top: 10px;
+`;
+
+const UserProfile = ({ setIsloggedIn }) => {
+  const onLogout = useCallback(() => {
+    setIsloggedIn(false);
+  }, []);
+
+  return (
+    <Card
+      actions={[
+        <div key='twit'>
+          띠링
+          <br />
+        </div>,
+        <div key='followings'>
+          팔로잉
+          <br />
+        </div>,
+        <div key='followers'>
+          팔로워
+          <br />
+        </div>,
+      ]}
+    >
+      <Card.Meta avatar={<Avatar>SL</Avatar>} title='seilylook'></Card.Meta>
+      <ButtonWrapper onSubmit={onLogout}>로그아웃</ButtonWrapper>
+    </Card>
+  );
+};
 
 export default UserProfile;
