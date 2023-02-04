4c4073ecac190f437a748297358aac9bca9fcc4c
seilylook <seilylook@naver.com>
2023-02-04 11:04:50 +0900

    responsive grid implementation in ./AppLayout.js

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9ea2d6e65b51fd9f82ae";
+/******/ 	var hotCurrentHash = "9ff4f529d2d2234816c2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import { Menu, Input, Row, Col } from 'antd';
 
 const AppLayout = ({ children }) => {
   return (
@@ -14,15 +14,35 @@ const AppLayout = ({ children }) => {
         </Menu.Item>
         <Menu.Item>
           <Link href='/profile'>
-            <a>Profile</a>
+            <a>프로필</a>
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href='/signup'>
-            <a>Sign Up</a>
+            <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href='https://github.com/seilylook'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            Made by Seilylook
+          </a>
+        </Col>
+      </Row>
       {children}
     </div>
   );
