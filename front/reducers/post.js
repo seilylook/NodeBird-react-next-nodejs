3d6c5a4972af0e5a5ac4568b07840fedc5cbca12
seilylook <seilylook@naver.com>
2023-02-14 11:38:49 +0900

    feat: use immer library to keep data immutablity

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -2,6 +2,7 @@
 // sequelize를 통한 참조 관계 데이터는 대문자로 변수명 설정
 
 import shortId from 'shortid';
+import produce from 'immer';
 
 export const initialState = {
   mainPosts: [
@@ -100,90 +101,69 @@ const dummyComment = (data) => ({
   },
 });
 
+// reducer: 이전 상태를 액션을 통해 다음 상태로 만들어 내느 함수
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_POST_REQUEST:
-      return {
-        ...state,
-        addPostLoading: true,
-        addPostDone: false,
-        addPostError: null,
-      };
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case ADD_POST_REQUEST:
+        draft.addPostLoading = true;
+        draft.addPostDone = false;
+        draft.addPostError = false;
+        break;
 
-    case ADD_POST_SUCCESS:
-      return {
-        ...state,
-        mainPosts: [dummyPost(action.data), ...state.mainPosts],
-        addPostLoading: false,
-        addPostDone: true,
-      };
+      case ADD_POST_SUCCESS:
+        draft.addPostLoading = false;
+        draft.addPostDone = true;
+        draft.mainPosts.unshift(dummyPost(action.data));
+        break;
 
-    case ADD_POST_FAILURE:
-      return {
-        ...state,
-        addPostLoading: false,
-        addPostError: action.error,
-      };
+      case ADD_POST_FAILURE:
+        draft.addPostLoading = false;
+        draft.addPostError = action.error;
+        break;
 
-    case REMOVE_POST_REQUEST:
-      return {
-        ...state,
-        removwPostLoading: true,
-        removwPostDone: false,
-        removwPostError: null,
-      };
+      case REMOVE_POST_REQUEST:
+        draft.removePostLoading = true;
+        draft.removePostDone = false;
+        draft.removePostError = null;
+        break;
 
-    case REMOVE_POST_SUCCESS:
-      return {
-        ...state,
-        mainPosts: state.mainPosts.filter((v) => v.id !== action.data),
-        removePostLoading: false,
-        removePostDone: true,
-      };
+      case REMOVE_POST_SUCCESS:
+        draft.removePostLoading = false;
+        draft.removePostDone = true;
+        draft.mainPosts = draft.mainPosts.filter((v) => v.id !== action.data);
+        break;
 
-    case REMOVE_POST_FAILURE:
-      return {
-        ...state,
-        removePostLoading: false,
-        removePostError: action.error,
-      };
+      case REMOVE_POST_FAILURE:
+        draft.removePostLoading = false;
+        draft.removePostError = action.error;
+        break;
 
-    case ADD_COMMENT_REQUEST:
-      return {
-        ...state,
-        addCommentLoading: true,
-        addCommentDone: false,
-        addCommentError: null,
-      };
+      case ADD_COMMENT_REQUEST:
+        draft.addCommentLoading = true;
+        draft.addCommentDone = false;
+        draft.addCommentError = null;
+        break;
 
-    case ADD_COMMENT_SUCCESS:
-      const postIndex = state.mainPosts.findIndex(
-        (v) => v.id === action.data.postId
-      );
-      const post = state.mainPosts[postIndex];
-      const Comments = [dummyComment(action.data.content), ...post.Comments];
-      post.Comments = post.Comments.slice();
+      // immer 사용해서 불변성 지켜주기
+      case ADD_COMMENT_SUCCESS:
+        // post = mainPosts에서 작성한 ID 찾기
+        const post = draft.mainPosts.find((v) => v.id === action.data.postId);
+        // post index의 Comments / content = action.data.content 넣기
+        post.Comments.unshift(dummyComment(action.data.content));
+        draft.addCommentLoading = false;
+        draft.addCommentDone = true;
+        break;
 
-      const mainPosts = [...state.mainPosts];
-      mainPosts[postIndex] = { ...post, Comments };
+      case ADD_COMMENT_FAILURE:
+        draft.addCommentLoading = false;
+        draft.addCommentError = action.error;
+        break;
 
-      return {
-        ...state,
-        mainPosts,
-        addCommentLoading: false,
-        addCommentDone: true,
-      };
-
-    case ADD_COMMENT_FAILURE:
-      return {
-        ...state,
-        addCommentLoading: false,
-        addCommentError: action.error,
-      };
-
-    default:
-      return state;
-  }
+      default:
+        break;
+    }
+  });
 };
 
 export default reducer;
