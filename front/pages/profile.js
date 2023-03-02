10810ebabc7b6f44167fffff390d9a8a4099f95e
seilylook <seilylook@naver.com>
2023-03-02 13:39:25 +0900

    REFACTOR: profile page loadFollowings/loadFollowers position change

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -22,12 +22,13 @@ const Profile = () => {
   const [followersLimit, setFollowersLimit] = useState(3);
   const [followingsLimit, setFollowingsLimit] = useState(3);
 
-  const { data: followersData, error: followerError } = useSWR(
-    `http://localhost:3065/user/followers?limit=${followersLimit}`,
-    fetcher
-  );
   const { data: followingsData, error: followingError } = useSWR(
     `http://localhost:3065/user/followings?limit=${followingsLimit}`,
+    fetcher
+  );
+
+  const { data: followersData, error: followerError } = useSWR(
+    `http://localhost:3065/user/followers?limit=${followersLimit}`,
     fetcher
   );
 
@@ -39,12 +40,12 @@ const Profile = () => {
     }
   }, [me && me.id]);
 
-  const loadMoreFollowers = useCallback(() => {
-    setFollowersLimit((prev) => prev + 3);
-  }, []);
-
   const loadMoreFollowings = useCallback(() => {
     setFollowingsLimit((prev) => prev + 3);
+  }, []);
+
+  const loadMoreFollowers = useCallback(() => {
+    setFollowersLimit((prev) => prev + 3);
   }, []);
 
   // 로그인 하지 않았을 때
